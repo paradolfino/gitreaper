2cfaaf9c77466f7cfa942436ad635095accdf4ba
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-26 21:24:14 -0600

    commit 2 to pool[serene-living-cow-p66pwf] at 21:24 - 26/12/2017

--- threader.rb
+++ threader.rb
@@ -1,17 +1,17 @@
 module Threader
     def Threader.bits
-       return ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+       ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     end
 
     def Threader.bits_adjs
-        return ['serene','rapid','brilliant','pretty']
+        ['serene','rapid','brilliant','pretty']
     end
 
     def Threader.bits_verbs
-        return ['rolling','living','shining','steaming']
+        ['rolling','living','shining','steaming']
     end
 
     def Threader.bits_nouns
-        return ['cow','rabbit','mountain','river']
+        ['cow','rabbit','mountain','river']
     end
 end
