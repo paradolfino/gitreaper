7c2b59141827031583364a75f3092bd12066febd
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-26 21:14:54 -0600

    commit 4 to pool[pretty-shining-mountain-egn76i] at 21:14 - 26/12/2017

---
+++ threader.rb
@@ -0,0 +1,4 @@
+bits = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+bits_adjs = ['serene','rapid','brilliant','pretty']
+bits_verbs = ['rolling','living','shining','steaming']
+bits_nouns = ['cow','rabbit','mountain','river']
