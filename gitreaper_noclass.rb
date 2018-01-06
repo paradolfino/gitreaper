fbc264851a7d5dfcc44c5e2535f958de8721562a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-05 21:42:50 -0600

    commit 1 to pool[rapid-flying-rivers-6dshcu] at 21:42 - 05/01/2018

--- gitreaper_noclass.rb
+++ gitreaper_noclass.rb
@@ -16,11 +16,11 @@ require 'pathname'
 require 'threader'
 
 
-color_red = "\033[31m"
-color_green = "\033[32m"
-color_default = "\033[0m"
-commits = 1
-time_running = 0
+$color_red = "\033[31m"
+$color_green = "\033[32m"
+$color_default = "\033[0m"
+$commits = 1
+$time_running = 0
 
 def execute(param)
     stalker = %x{#{param}}
