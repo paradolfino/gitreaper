bd824d34608c3dfc0a14605725b89f5ead18c8b3
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-06 15:02:31 -0600

    commit 3 to pool[lovely-shining-mountains-wzswj3] at 15:02 - 06/01/2018

--- gitreaper_slim.rb
+++ gitreaper_slim.rb
@@ -1,6 +1,7 @@
 =begin
 
-    Hi there. This is a slimmed down version of GitReaper.
+    Hi there. This is a slimmed down version of GitReaper. By slim I mean that there isn't a threader, there isn't any coloring, limited output
+    and only the exit options. Commit pools will be using only a 6 character alphanumeric id instead of "serene-something-something".
     -Thanks
     Viktharien Volander
 
