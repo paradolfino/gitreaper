88fae9f84344275aa71cc06f23a79c773e81dea6
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-08 15:41:25 -0600

    commit 11 to pool[lovely-jumping-cabins-05l01r] at 15:41 - 08/01/2018

--- gr_gui.rb
+++ gr_gui.rb
@@ -2,8 +2,10 @@ Shoes.app do
     background white
         stack(margin: 20) {
           @test = button "A bed of clams"
+          @out = para "Test"
         }
         @test.click {
             reaper = %x{ls}
+            @out.replace "Test"
         }
 end
