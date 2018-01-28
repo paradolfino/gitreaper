304e065b5a858ae4472c35ede75bad57d115421a
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 18:08:50 -0600

    commit 1 to pool[rapid-shining-rivers-i40d68] at 18:08 - 27/01/2018

--- gr_gui.rb
+++ gr_gui.rb
@@ -1,11 +1,6 @@
-Shoes.app do
-    background white
-        stack(margin: 20) {
-          @test = button "A bed of clams"
-          @out = para "Test"
-        }
-        @test.click {
-            reaper = %x{ls}
-            @out.replace "Test22"
-        }
-end
+Shoes.app {
+    t = `pwd`
+    @push = button "Push me"
+    @note = para t
+    
+  }
