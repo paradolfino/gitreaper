97429419613c5b8e9073ee0281f1b3c8e0737e84
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 18:13:10 -0600

    commit 7 to pool[rapid-shining-rivers-i40d68] at 18:13 - 27/01/2018

--- gr_sandbox.rb
+++ gr_sandbox.rb
@@ -4,7 +4,7 @@ def output input
     changes = input.strip.split(",")
     changes.map! {|item| item = "* #{item.strip}"}
     
-    open('why_commit.txt', 'w') do |file|
+    open('pull.txt', 'w') do |file|
         puts "pool[test]:"
         puts changes
     end
