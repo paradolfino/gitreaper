9868a838dbda24e6a7e2a6afe8984d05c3a58c6c
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 18:20:52 -0600

    commit 1 to pool[rapid-shining-mountains-hym68r] at 18:20 - 27/01/2018

--- gr_sandbox.rb
+++ gr_sandbox.rb
@@ -1,14 +1,14 @@
 #testing new features here
 
-def output input
-    changes = input.strip.split(",")
-    changes.map! {|item| item = "* #{item.strip}"}
+# def output input
+#     changes = input.strip.split(",")
+#     changes.map! {|item| item = "* #{item.strip}"}
     
-    open('pull_me.txt', 'w') do |file|
-        file.puts "### pool[test]:"
-        file.puts changes
-    end
-end
-input = gets.chomp
+#     open('pull_me.txt', 'w') do |file|
+#         file.puts "### pool[test]:"
+#         file.puts changes
+#     end
+# end
+# input = gets.chomp
   
-output input
+# output input
