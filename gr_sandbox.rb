2f64fc3f43c91fcda72f6c9d1a59a86dcab6b7ad
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2018-01-27 18:09:41 -0600

    commit 4 to pool[rapid-shining-rivers-i40d68] at 18:09 - 27/01/2018

--- gr_sandbox.rb
+++ gr_sandbox.rb
@@ -1,1 +1,11 @@
 #testing new features here
+
+def output input
+    changes = input.strip.split(",")
+    changes.map! {|item| item = "* #{item.strip}"}
+    puts "pool[test]:"
+    puts changes
+end
+    input = gets.chomp
+    
+    output input
